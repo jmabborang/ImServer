336e4d887ba2149e1f736046f899733faff87a82
Jeffrey Mabborang <jeffrey.mabborang@jeonsoft.com>
2024-12-25 01:33:47 +0800

    Secure Authentication Using CSRF Token

--- router/index.js
+++ router/index.js
@@ -1,6 +1,8 @@
+const tokenRoute = require('./token')
 const userLoginRoute = require('./login')
 
 const mountRoutes = (app) => {
+    app.use('/', tokenRoute)
     app.use('/userLogin', userLoginRoute)
 }
 
