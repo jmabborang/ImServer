3088fc1688481265b2cfc5299ada9bce22fe8c60
Jeffrey Mabborang <jeffrey.mabborang@jeonsoft.com>
2025-01-24 17:25:28 +0800

    Remove Exess space

--- router/index.js
+++ router/index.js
@@ -2,10 +2,6 @@ const tokenRoute = require('./token')
 const userLoginRoute = require('./login')
 
 const mountRoutes = (app) => {
-    
-
-
-    
     app.use('/', tokenRoute)
     app.use('/userLogin', userLoginRoute)
 }
