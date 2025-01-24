ad307c4f2d8082d039130363035174bd8ad3c061
Jeffrey Mabborang <jeffrey.mabborang@jeonsoft.com>
2025-01-24 15:57:37 +0800

    Test 2

--- router/index.js
+++ router/index.js
@@ -2,6 +2,7 @@ const tokenRoute = require('./token')
 const userLoginRoute = require('./login')
 
 const mountRoutes = (app) => {
+    
     app.use('/', tokenRoute)
     app.use('/userLogin', userLoginRoute)
 }
